ac9252716d0a4f5a34c9ef5c1c6de6e8267cb2a2
zero <teclaizerai@gmail.com>
2024-04-14 17:09:14 +0200

    Tool: update rector to v1.0.4.

--- rector.php
+++ rector.php
@@ -2,9 +2,9 @@
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
-use Rector\Core\ValueObject\PhpVersion;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Symfony\Set\SymfonySetList;
+use Rector\ValueObject\PhpVersion;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
